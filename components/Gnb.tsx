b9f51c3540d6dde6d8ba1d968e5c5e8960f3be12
Jun Son <sonjun461245@naver.com>
2022-11-08 21:49:06 +0900

    라우팅 명칭 변경 ranking -> goods

--- components/Gnb.tsx
+++ components/Gnb.tsx
@@ -6,7 +6,7 @@ const Gnb = () => {
         <div className="font-Pretendard">
            <nav className="bg-slate-200 md:mx-auto flex flex-wrap items-center justify-center h-14">
                 <Link href="/" legacyBehavior><a className="mr-5 hover:text-blue-600 active:">홈</a></Link>
-                <Link href="/ranking" legacyBehavior><a className="mr-5 hover:text-blue-600">상품</a></Link>
+                <Link href="/goods" legacyBehavior><a className="mr-5 hover:text-blue-600">상품</a></Link>
                 <Link href="/board" legacyBehavior><a className="mr-5 hover:text-blue-600">게시판</a></Link>
             </nav>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,19 +12,7 @@ const Home: NextPage = () => {
       </Head>
       <Headers />
       <Gnb />
-      {/* <div className=" flex px-5 py-24 items-center justify-center flex-col">
-        <Image className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
-          alt="hero"
-          src="https://dummyimage.com/1280x720"
-          width={1280}
-          height={720} />
-        <div className="text-center lg:w-2/3 w-full">
-          <p className="mb-8 leading-relaxed">여기에 코디를 원하는 옷 이미지를 Drag&Drop하고 메칭하기 버튼을 눌러주세요!</p>
-          <div className="flex justify-center">
-            <button className="inline-flex text-white bg-slate-700 border-0 py-2 px-6 focus:outline-none hover:bg-slate-900 rounded text-lg">Button</button>
-          </div>
-        </div>
-      </div> */}
+      
     </>
   )
 }
