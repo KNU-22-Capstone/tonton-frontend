29ee606860f90d4e6fff3bb33664b35593a9b0a5
Jun Son <sonjun461245@naver.com>
2022-11-11 21:46:21 +0900

    Update register.tsx

--- pages/register.tsx
+++ pages/register.tsx
@@ -8,6 +8,7 @@ const register = () => {
             <Head>
                 <title>TONTON - 회원가입</title>
             </Head>
+            
             <section className="bg-gray-50 dark:bg-gray-900 font-Pretendard">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-slate-900 md:text-2xl dark:text-white text-center pb-5">
