5b2ead6287fc5391bce52e921357acba325f7cbf
Jun Son <sonjun461245@naver.com>
2022-11-17 02:10:11 +0900

    axios test url 수정

--- pages/login.tsx
+++ pages/login.tsx
@@ -22,7 +22,7 @@ const login = () => {
   };
 
   const postTest = async ()=>{
-      axios.post('localhost:8080>/api/v1', {
+      axios.post('http://localhost:8080/api/v1', {
       loginId: id,
       password: password
     })
