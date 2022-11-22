835ba047b132e253be54c92a5035d69598ca5fdb
Jun Son <sonjun461245@naver.com>
2022-11-23 03:08:11 +0900

    텍스트 위치 및 문구 추가

--- components/ImageInput.tsx
+++ components/ImageInput.tsx
@@ -3,11 +3,9 @@ import React, { useCallback, useRef, useState } from "react";
 
 const ImageInput = () => {
   const fileRef = useRef<HTMLInputElement>(null);
-  const [images, setImages] = useState<string>("");
+  const [images, setImages] = useState<string>();
   const [imageBoolean, setImageBoolean] = useState<boolean>(false);
-
-  const buttn_name: string[] = ["모자", "상의", "하의", "신발", "아우터"];
-  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const TagButtonName: string[] = ["모자", "상의", "하의", "신발", "아우터"];
 
   const handleClick = () => {
     fileRef?.current?.click();
@@ -19,13 +17,24 @@ const ImageInput = () => {
   };
 
   const handleCancel = (e: any) => {
-    setImages("");
+    window.location.reload();
     setImageBoolean(false);
+  };
+
+  const handle_TagButtonClock = () => {
+    
   }
 
   return (
     <div className="font-Pretendard ">
-      <div className="flex items-center justify-center flex-row m-5">
+       <div className={`${imageBoolean ? 'hidden' : ''} text-center mt-6 text-2xl`}>
+       <span className="font-bold">코디</span><span> 아직도 망설이시나요?</span>
+        <br/>
+        <span className="font-bold">이미지 업로드</span><span>를 클릭해 코디를 원하는</span>
+        <br/>
+        <span>옷 이미지를 업로드 해 보세요.</span>
+      </div>
+      <div className="flex items-center justify-center flex-row m-2">
         <div className="">
           <input
             ref={fileRef}
@@ -40,7 +49,7 @@ const ImageInput = () => {
           <button
             className={`${
               imageBoolean ? "hidden" : ""
-            } text-2xl my-10 w-[600px] h-[600px] border-dashed border-2 rounded border-slate-900`}
+            } text-2xl my-5 w-[600px] h-[600px] border-dashed border-2 rounded border-slate-900`}
             onClick={handleClick}
           >
             이미지 업로드
@@ -55,25 +64,36 @@ const ImageInput = () => {
             />
           )}
         </div>
-        <div className={`${imageBoolean ? '' : 'hidden'} flex flex-col h-[600px] justify-center items-center ml-5`}>
+        <div
+          className={`${
+            imageBoolean ? "" : "hidden"
+          } flex flex-col h-[600px] justify-center items-center ml-5`}
+        >
           <div className="text-3xl py-5">원하는 태그를 선택해 주세요.</div>
           <div className="grid grid-cols-5 gap-4 p-2">
-            <button className=" bg-white border-gray-300 text-gray-400 rounded shadow border w-28 h-9">
-              #모자
-            </button>
+                {TagButtonName.map((tagButton, i) => (
+                <button
+                  key={i}
+                  onClick={handle_TagButtonClock}
+                  className= "bg-white border-gray-300 text-gray-400 rounded shadow border w-28 h-9"
+                >{`#${tagButton}`}</button>
+              ))}
             <button className=" bg-white border-blue-400 text-blue-400 rounded shadow border w-28 h-9">
               #상의
             </button>
           </div>
           <div className="w-full font-bold mt-auto">
-            <button className="bg-slate-800 text-white w-full p-2 rounded justify-end">메칭하기</button>
-            <button onClick={handleCancel} className="bg-rose-700 text-white w-full p-2 rounded justify-end">취소하기</button>
+            <button className="bg-slate-800 text-white w-full p-2 rounded">
+              메칭하기
+            </button>
+            <button
+              onClick={handleCancel}
+              className="bg-rose-700 text-white w-full p-2 rounded"
+            >
+              취소하기
+            </button>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center">
-        <h1 className="text-3xl font-bold">코디</h1>
-        <div className="text-3xl">를 원하는 옷 이미지를 업로드해 보세요.</div>
       </div>
     </div>
   );
