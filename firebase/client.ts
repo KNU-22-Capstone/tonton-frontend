48729315fa82c2489ba16e6e835a1eaf33b3a708
Jun Son <sonjun461245@naver.com>
2022-11-23 03:07:08 +0900

    .env로 api키 블록

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,29 +1,17 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-
-
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyBfLrWF_p_wxib7afnyP_9Pz_Qdk4iskEI",
-  //   authDomain: "tttest-95867.firebaseapp.com",
-  //   projectId: "tttest-95867",
-  //   storageBucket: "tttest-95867.appspot.com",
-  //   messagingSenderId: "42474123501",
-  //   appId: "1:42474123501:web:89b6a1b0620dfcf1699ce4",
-  //   measurementId: "G-QVQSQ8F2F4"
-  // };
   
-  const firebaseConfig = {
-    apiKey: "AIzaSyCEp0kWaVrGnYZ32QybC9eQyXk4Z2v4es4",
-    authDomain: "codiwiki-tonton.firebaseapp.com",
-    projectId: "codiwiki-tonton",
-    storageBucket: "codiwiki-tonton.appspot.com",
-    messagingSenderId: "1012508499472",
-    appId: "1:1012508499472:web:978acfceba2391f5277495",
-    measurementId: "G-84X1G7CG66"
-  };
+  const firebaseConfig_env = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID
+  }
 
-
-  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+  const app = !getApps().length ? initializeApp(firebaseConfig_env) : getApp();
   export const db = getFirestore(app);  
   
 
