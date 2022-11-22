260812411db625d88cd8197fce27baca404f0db7
Jun Son <sonjun461245@naver.com>
2022-11-23 05:02:16 +0900

    type 수정 & img 링크 추가.

--- components/MainPage_Components/Main_Ranking_Card.tsx
+++ components/MainPage_Components/Main_Ranking_Card.tsx
@@ -1,31 +1,41 @@
 import React, { useCallback, useDebugValue, useState } from "react";
 import { useEffect } from "react";
-import { collection, getDocs, where, query, orderBy, limit } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  where,
+  query,
+  orderBy,
+  limit,
+} from "firebase/firestore";
 import { db } from "../../firebase/client";
 import { DocumentData } from "firebase/firestore";
 
 const Main_Rank_Card = () => {
   type clothType = {
-    link: string;
-    price: string;
-    title: string;
-    imageIRL: string;
+    detail_tag: string;
+    major_tag: string;
+    name: string;
+    picture_URL: string;
+    price: number;
+    site_URL: string;
+    site_name: string;
+    views: number;
   };
 
   const [clothes, setClothes] = useState<clothType[]>([]);
   const arr: clothType[] = [];
-  const usersCollectionRef = collection(db, "테스트2");
+  const usersCollectionRef = collection(db, "의류");
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const q  = await query(
-        usersCollectionRef,
-        //where("tag", "==", "무신사"),
-        orderBy("title", "asc"),
-        limit(10)
+    const q = await query(
+      usersCollectionRef,
+      //where("tag", "==", "무신사"),
+      orderBy("name", "asc"),
     );
 
     const data = await getDocs(q);
@@ -39,9 +49,10 @@ const Main_Rank_Card = () => {
     <div className="font-Pretendard flex flex-row justify-center items-center m-10">
       <div>
         {clothes.map((cloth, i) => (
-          <div
-            key={i}
-          >{`이름: ${cloth.title} 가격: ${cloth.price}`}</div>
+          <div key={i}>
+            {`이름: ${cloth.name}`}
+            <img src={cloth.picture_URL}/>
+          </div>
         ))}
       </div>
     </div>
